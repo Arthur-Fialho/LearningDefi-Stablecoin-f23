19b373705841189f9b8cb7fd340573b8f1cf5687
Arthur-Fialho <arthursouza.fp@gmail.com>
2024-04-10 17:49:53 -0300

    initial commit

---
+++ test/DecentralizedStableCoinTest.t.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+
+contract DecentralizedStableCoinTest is Test {
+    DecentralizedStableCoin dsc;
+
+    function setUp() public {
+        dsc = new DecentralizedStableCoin(msg.sender);
+    }
+
+    function testMustMintMoreThanZero() public {
+        vm.prank(dsc.owner());
+        vm.expectRevert();
+        dsc.mint(address(this), 0);
+    }
+
+    function testMustBurnMoreThanZero() public {
+        vm.startPrank(dsc.owner());
+        dsc.mint(address(this), 100);
+        vm.expectRevert();
+        dsc.burn(0);
+        vm.stopPrank();
+    }
+
+    function testCantBurnMoreThanYouHave() public {
+        vm.startPrank(dsc.owner());
+        dsc.mint(address(this), 100);
+        vm.expectRevert();
+        dsc.burn(101);
+        vm.stopPrank();
+    }
+
+    function testCantMintToZeroAddress() public {
+        vm.startPrank(dsc.owner());
+        vm.expectRevert();
+        dsc.mint(address(0), 100);
+        vm.stopPrank();
+    }
+}
